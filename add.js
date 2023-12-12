d44fa6f2a3462102870acd579735546bc3668d5f
FH Himel <fhhimel17@gmail.com>
2023-12-13 00:45:31 +0600

    added-05

---
+++ add.js
@@ -0,0 +1,10 @@
+function add(a,b,c,d){
+    let sum = a + b + c + d;
+    return sum;
+}
+let num1 = 4;
+let num2 = 8;
+let num3 = 6;
+let num4 = 5;
+
+console.log(add(num1, num2, num3, num4));
---
+++ function-exmpl.js
@@ -0,0 +1,12 @@
+function avg(a,b,c,d){
+    let total = a + b + c + d;
+    let average = total/4;
+    return average;
+}
+let xm1 = 55;
+let xm2 = 35;
+let xm3 = 45;
+let xm4 = 59;
+
+
+console.log(avg(xm1, xm2, xm3, xm4));
